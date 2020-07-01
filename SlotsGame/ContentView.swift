2bdee2b6624792c2d80c099bed07e8324fb1241e
adamghaffarian <mailadamg@gmail.com>
2020-07-01 15:37:49 -0400

    Completed up to tutorial initial project

--- SlotsGame/ContentView.swift
+++ SlotsGame/ContentView.swift
@@ -9,8 +9,123 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var credits: Int = 100
+    @State private var slotState = [0, 1, 2]
+    
+    private var symbols = ["apple", "cherry", "star"]
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color(red: 200/233, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .foregroundColor(Color(red: 228/233, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Spacer()
+                TitleView()
+                Spacer()
+                CreditsView(credits: $credits)
+                Spacer()
+                SlotsView(symState: $slotState, symbols: symbols)
+                Spacer()
+                Spacer()
+                SpinButton(credits: $credits, slotState: $slotState)
+                Spacer()
+                }.padding(.all)
+
+        }
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+            Text("SwiftUI Slots")
+                .bold()
+                .foregroundColor(.white)
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+        }.font(.title)
+    }
+}
+
+struct CreditsView: View{
+    @Binding var credits: Int
+    var body: some View{
+        Text("Credits: " + String(credits))
+            .foregroundColor(.black)
+            .padding(.all)
+            .background(Color.white.opacity(0.5))
+            .cornerRadius(20)
+    }
+}
+
+struct SlotsView: View {
+    @Binding var symState: [Int]
+    var symbols: [String]
+    var body: some View{
+        HStack {
+            SlotCard(imageName: symbols[symState[0]])
+            SlotCard(imageName: symbols[symState[1]])
+            SlotCard(imageName: symbols[symState[2]])
+        }
+    }
+}
+
+struct SlotCard: View {
+    var imageName: String
+    
+    var body: some View {
+        Image(imageName).resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .background(Color.white.opacity(0.5))
+            .cornerRadius(20)
+    }
+}
+
+struct SpinButton: View {
+    @Binding var credits: Int
+    @Binding var slotState: [Int]
+    let winnings = 50
+    let cost = 5
+    var body: some View{
+        Button(action: {
+            // Check Credits
+            if self.credits < self.cost{
+                // Not enough credits
+                return
+            }
+            
+            // Randomize Slots
+            self.slotState[0] = Int.random(in: 0...self.slotState.count-1)
+            self.slotState[1] = Int.random(in: 0...self.slotState.count-1)
+            self.slotState[2] = Int.random(in: 0...self.slotState.count-1)
+            
+            // Check Slots
+            if self.slotState[0] == self.slotState[1] &&
+                self.slotState[1] == self.slotState[2]{
+                // Match / Win
+                self.credits += self.winnings
+            } else {
+                // No Match / Lose
+                self.credits -= self.cost
+            }
+        }){
+            Text("Spin")
+                .bold()
+                .foregroundColor(.white)
+                .padding(.all, 10)
+                .padding(.horizontal, 30)
+                .background(Color.pink)
+                .cornerRadius(10)
+        }
     }
 }
 
@@ -19,3 +134,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
