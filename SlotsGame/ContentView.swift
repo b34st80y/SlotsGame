0043a097a7f3782177f17f8ccec6fbb38acea861
adamghaffarian <mailadamg@gmail.com>
2020-07-01 14:11:14 -0400

    Initial Commit

---
+++ SlotsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsGame
+//
+//  Created by Adam Ghaffarian on 7/1/20.
+//  Copyright © 2020 Adam Ghaffarian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
